ae372d94fcd2904bbb0a9ce71f8b2d1a91beab27
jiemozhuzhu <jiemo@localhost.localdomain>
2019-12-01 03:23:14 -0800

    first version

---
+++ app/Article/Article.php
@@ -0,0 +1,35 @@
+<?php
+namespace Lafen\Article;
+
+class Article{
+	public $name;
+
+
+	public function __construct(){
+
+        
+        }
+
+
+	public function add(){
+
+	}
+
+	public function edit(){
+		
+	}
+
+	public function del(){
+		
+	}
+
+
+	public function editor(){
+
+	}
+
+	public function show(){
+		echo "show";
+	}
+
+}
